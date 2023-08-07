1e947a6acffdf09202a0533a6956425bf335f7e9
NithinU2802 <nithinu2810@gmail.com>
2023-08-07 19:38:14 +0530

    Day 1 of JavaScript

---
+++ Challenge/Day-1/HelloWorld.js
@@ -0,0 +1,15 @@
+// way to print hello world 
+
+console.log("Hello World")
+
+
+// let Name='Nithin'
+// let age=21
+// let isLearning=true 
+// let score=8.58
+
+// Elements are displayed on console
+console.log("Name: ",Name)
+console.log("Age: ",age)
+console.log("Learning: ",isLearning)
+console.log("Score: ",score)
---
+++ Challenge/Day-1/Variable.js
@@ -0,0 +1,17 @@
+// Variable declaration can be implemented through two keyword let and const
+
+// usage of let
+let Name='Nithin'
+let age=21
+let isLearning=true 
+let score=8.58
+
+// usage of const
+let Name1='Nithin'
+let age1=21
+let isLearning1=true 
+let score1=8.58
+
+// ways of declaring variables
+let name='Nithin',designation='Student',city='Trichy',three=8.4
+const one=34,two=34.4,bool=false
